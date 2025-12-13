f11c008d62dc42cec259763ae2f803c2c5b44b92
alibazregarijs <alibazregariintel@gmail.com>
2025-12-13 10:41:10 +0330

    SCRUM-14 refactor(ui): mobile-nav-link now has it's own component

---
+++ apps/front-end/src/app/(dashboard)/dashboard/learner/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LearnerLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default LearnerLayout;
--- apps/front-end/src/features/dashboard/learner/components/LearnerNavbar.tsx
+++ apps/front-end/src/features/dashboard/learner/components/LearnerNavbar.tsx
@@ -1,58 +1,46 @@
 'use client';
-
 import React from 'react';
+import Navbar from '@src/shared/components/layouts/NavbarCompound';
 import Link from 'next/link';
 import HomeFilledIcon from '@mui/icons-material/HomeFilled';
-
-// Shared Components
 import Text from '@shared/components/ui/atoms/Text/Text';
+import { navItems } from '@src/shared/constants';
 import Button from '@shared/components/ui/atoms/Button/Button';
-import { navItems } from '@shared/constants';
 import { MobileNavLink } from '@src/shared/components/layouts/MobileNavLink';
-// The Compound Component
-import Navbar from '@shared/components/layouts/NavbarCompound';
+import Searchbar from '@src/shared/components/ui/molecules/Searchbar/Searchbar';
+import Notification from '@src/shared/components/ui/atoms/Notification/Notification';
 
-// Helper: A Link that closes the menu when clicked
-
-const LandingNavbar = () => {
+const LearnerNavbar = () => {
   return (
     <Navbar>
       {/* --- TOP BAR --- */}
       <Navbar.Container>
         {/* BRAND */}
-        <Navbar.Brand>
+        <Navbar.Brand className="gap-4">
           <HomeFilledIcon fontSize="large" className="text-green-light-100" />
           <Text as="h1" className="font-inter ml-4 text-2xl font-bold text-white">
             Wiso-Tech
           </Text>
+          <Navbar.Desktop>
+            {navItems.map((item) => (
+              <Link key={item.name} href={item.href}>
+                <Text
+                  as="span"
+                  className="text-md text-gray-light-100 transition-colors hover:text-white"
+                >
+                  {item.name}
+                </Text>
+              </Link>
+            ))}
+          </Navbar.Desktop>
         </Navbar.Brand>
 
         {/* DESKTOP NAV (Hidden on Mobile) */}
-        <Navbar.Desktop>
-          {navItems.map((item) => (
-            <Link key={item.name} href={item.href}>
-              <Text
-                as="span"
-                className="text-md text-gray-light-100 transition-colors hover:text-white"
-              >
-                {item.name}
-              </Text>
-            </Link>
-          ))}
-        </Navbar.Desktop>
 
         {/* DESKTOP ACTIONS (Hidden on Mobile) */}
-        <Navbar.Desktop className="space-x-8">
-          <Link href="/sign-in">
-            <Text as="span" className="text-md cursor-pointer text-white">
-              Sign In
-            </Text>
-          </Link>
-          <Link href="/sign-up">
-            <Button variant="regular" color="primary">
-              Sign Up
-            </Button>
-          </Link>
+        <Navbar.Desktop className="">
+          <Searchbar />
+          <Notification isNotifications={false} />
         </Navbar.Desktop>
 
         {/* MOBILE TOGGLE (Visible on Mobile) */}
@@ -91,4 +79,4 @@ const LandingNavbar = () => {
   );
 };
 
-export default LandingNavbar;
+export default LearnerNavbar;
