de38d37442ac49e53be72bf0e8499a9781d818b6
alibazregarijs <alibazregariintel@gmail.com>
2025-12-16 17:31:57 +0330

    SCRUM-12 chore: in-progress section done

--- apps/front-end/src/features/dashboard/learner/components/LearnerInProgress.tsx
+++ apps/front-end/src/features/dashboard/learner/components/LearnerInProgress.tsx
@@ -1,6 +1,9 @@
 'use client';
+import Image from 'next/image';
 import React from 'react';
 
+import { inProgressItems } from '@src/features/dashboard/learner/constants';
+import Button from '@src/shared/components/ui/atoms/Button/Button';
 import Text from '@src/shared/components/ui/atoms/Text/Text';
 import ProgressBar from '@src/shared/components/ui/molecules/ProgressBar/ProgressBar';
 
@@ -11,31 +14,43 @@ const LearnerInProgress = () => {
         In Progress
       </Text>
       <div className="grid grid-cols-2">
-        <div className="col-span-1 flex flex-col gap-4 rounded-md border-[0.1px] border-gray-600 p-4">
-          <div className="relative aspect-video w-full overflow-hidden rounded-md">
-            {/* <Image
-              src={uiImage}
-              alt="Picture of the author"
-              fill={true}
-              className="object-cover"
-              priority={false}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            /> */}
-          </div>
+        {inProgressItems.map((item) => (
+          <div
+            key={item.id}
+            className="col-span-1 flex flex-col gap-4 rounded-md border-[0.1px] border-gray-600 p-4"
+          >
+            <div className="relative aspect-video w-full overflow-hidden rounded-md">
+              <Image
+                src={item.image}
+                alt="Picture of the author"
+                fill={true}
+                className="object-cover"
+                priority={false}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div>
 
-          <div className="flex flex-col">
-            <Text as="h2" className="font-inter text-xl font-bold text-white">
-              Advanced CSS and Sass
-            </Text>
-            <Text as="p" className="font-inter text-gray-light-100 text-sm">
-              by john schmedtmann
-            </Text>
+            <div className="flex flex-col">
+              <Text as="h2" className="font-inter text-xl font-bold text-white">
+                {item.title}
+              </Text>
+              <Text as="p" className="font-inter text-gray-light-100 text-sm">
+                {item.author}
+              </Text>
+            </div>
+            <div>
+              <ProgressBar>
+                <ProgressBar.Footer
+                  value={item.progress}
+                  text={item.completed ? 'Completed' : 'In Progress'}
+                />
+              </ProgressBar>
+            </div>
+            <div className="flex w-full flex-col">
+              <Button variant="regular">Continue Learning</Button>
+            </div>
           </div>
-          <ProgressBar>
-            <ProgressBar.Footer value={80} text="Completed" />
-          </ProgressBar>
-        </div>
-        <div className="col-span-1" />
+        ))}
       </div>
     </div>
   );
---
+++ apps/front-end/src/features/dashboard/learner/constants/index.ts
@@ -0,0 +1,21 @@
+import cssImage from '@src/public/learner/css2.webp';
+import uiImage from '@src/public/learner/ui.webp';
+
+export const inProgressItems = [
+  {
+    id: 'progress-1',
+    title: 'Advanced CSS and Sass',
+    author: 'john schmedtmann',
+    image: uiImage,
+    progress: 80,
+    completed: true,
+  },
+  {
+    id: 'progress-2',
+    title: 'UI/UX Design Principles',
+    author: 'john Doe',
+    image: cssImage,
+    progress: 25,
+    completed: true,
+  },
+];
